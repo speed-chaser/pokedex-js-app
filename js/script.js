ddaca9b4a341e6f07bab5b44da3abdcc5cb97a7a
speed-chaser <aychasepro@gmail.com>
2023-06-26 15:11:59 -0400

    Created and added first 9 Pokemon to pokemonList array in script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-alert('Chase was here');
+let pokemonList = [
+    { dexNumber: 1, name: 'Bulbasaur', types: ['grass', 'poison'], height: .7},
+    { dexNumber: 2, name: 'Ivysaur', types: ['grass', 'poison'], height: 1},
+    { dexNumber: 3, name: 'Venusaur', types: ['grass', 'poison'], height: 2},
+    { dexNumber: 4, name: 'Charmander', types: ['fire'], height: .6},
+    { dexNumber: 5, name: 'Charmeleon', types: ['fire'], height: 1.1},
+    { dexNumber: 6, name: 'Charizard', types: ['fire'], height: 1.7},
+    { dexNumber: 7, name: 'Squirtle', types: ['water'], height: .5},
+    { dexNumber: 8, name: 'Wartortle', types: ['water'], height: 1},
+    { dexNumber: 9, name: 'Blastoise', types: ['water'], height: 1.6},
+];
 
-const favoriteFood = "Spaghetti";
-document.write(favoriteFood);
+console.log(pokemonList);
